8a74abfeb869fe90da5a23999767e630a204b518
Assuéria <ellyassueria@gmail.com>
2023-03-18 17:25:47 -0300

    Alteração do favicon

--- components/footer/Newsletter.tsx
+++ components/footer/Newsletter.tsx
@@ -2,14 +2,13 @@ import Text from "$store/components/ui/Text.tsx";
 
 function Newsletter() {
   return (
-    <div class="flex flex-col sm:flex-row items-center gap-6 sm:gap-20">
-      <div class="flex flex-col gap-2 max-w-[400px]">
+    <div class="flex  sm:flex-row items-center gap-6 sm:gap-20">
+      <div class="flex gap-2 max-w-[400px]">
         <Text variant="heading-2" tone="default-inverse">
           Cadastre-se
         </Text>
         <Text variant="caption" tone="default-inverse">
-          Fique por dentro das novidades e ganhe 15% de desconto na primeira
-          compra. Para mais informações clique aqui.
+        Na Loja Oficial do Flamengo, você encontra produtos exclusivos para torcer, comemorar e representar todo o orgulho e paixão Rubro-Negra.
         </Text>
       </div>
       <form class="flex flex-row items-center gap-2 font-body text-body w-full sm:w-[408px]">
