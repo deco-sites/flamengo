cd1361d0f6f405834b6544fe108471102c942b6f
Assuéria <ellyassueria@gmail.com>
2023-03-18 22:00:19 -0300

    Ajuste no navbar

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -2,6 +2,7 @@ import Header from "$store/components/header/Header.tsx";
 import type { Props } from "$store/components/header/Header.tsx";
 
 function HeaderSection(props: Props) {
+
   return <Header {...props} />;
 }
 
--- twind.config.ts
+++ twind.config.ts
@@ -111,7 +111,7 @@ const options: Omit<Options, "selfURL"> = {
       },
     },
     fontFamily: {
-      sans: ["Albert Sans", "sans-serif"],
+      sans: ["Arial Narrow", "sans-serif"],
       serif: ["inherit", "serif"],
     },
     screens: {
